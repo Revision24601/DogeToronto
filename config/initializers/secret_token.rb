32dce32a09af5a37bc538fefd1e9e6a9002c546c
DX113-XL <dx113-xl@dx-113.local>
2014-02-21 11:27:10 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Funwebapp::Application.config.secret_key_base = 'a9664ec86781a8576a4075af3acacd1f3b563681a05a37c177a3f3b479382b97ba81d372f02c6a03ebdcce80c76ee3897f491d5e5a43e6133138454a5b59edd9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Funwebapp::Application.config.session_store :cookie_store, key: '_funwebapp_session'
